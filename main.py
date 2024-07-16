4354cf444b4b99e7df9ad715c52063ab33d1cc3e
frensch <thiago.frensch@gmail.com>
2024-07-16 12:17:04 -0300

    organizando parametros

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import numpy as np
 import cv2
-import math
 
-path = '/Users/thiago.frensch/Downloads/PNG2'
-colordef = 4
+input_filename = '/Users/thiago.frensch/Downloads/PNG2/splah_bola_'
+extension = 'png'
+output_file = '/Users/thiago.frensch/Downloads/sprite.png'
+filename_zeros = 5
 columns = 5
 rows = 12
-width = 360
-height = 196
-size = height*rows, width*columns, colordef
+color_channels = 4
+
+file = input_filename + str(0).zfill(filename_zeros) + "." + extension
+img = cv2.imread(file, cv2.IMREAD_UNCHANGED)
+height = img.shape[0]
+width = img.shape[1]
+
+size = height*rows, width*columns, color_channels
 m = np.zeros(size, dtype=np.uint8)
 icol = 0
 irow = 0
 for item in range(0,columns*rows,1):
-	file = path + '/splah_bola_' + ("%05d.png" % (item))
+	file = path + filename + str(item).zfill(filename_zeros) + "." + extension
+	print (file) 
 	img = cv2.imread(file, cv2.IMREAD_UNCHANGED)
-	print (item)
 	m[irow*height:irow*height + height, icol*width:icol*width + width] = img
 	icol += 1
 	if icol >= columns :
 		icol = 0
 		irow += 1
-	#cv2.imshow('image',m)
-	#cv2.waitKey(0)
 	
-cv2.imwrite('sprite.png', m[:,:,0:colordef])
+cv2.imwrite(output_file, m[:,:,0:color_channels])
